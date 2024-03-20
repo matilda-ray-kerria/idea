cc70343478b32e759927c95efa65b266abd306ce
matilda-ray-kerria <156324339+matilda-ray-kerria@users.noreply.github.com>
2024-03-20 16:16:53 +0900

    fix

--- doc/validate/integer_validator.rb
+++ doc/validate/integer_validator.rb
@@ -1,10 +1,10 @@
 def validate_integer_with_digit(value, digit = 6)
   if value.class != Integer
-    v = value.to_s.match(/^-?\d[_\d]*(.[_0]*)?$/){|v| v[0]}
+    v = value.to_s.match(/^-?\d[_\d]*(\.[_0]*)?$/){|v| v[0]}
     return false if v.nil?
 
     value = v.to_i
   end
 
-  value.abs < 10**digit ? true : false
+  value.abs < 10**digit
 end
